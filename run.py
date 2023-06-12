37d73b93256418806a9f06461a474e492d6f665b
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2023-06-12 16:19:35 +0000

    update stock sheet

--- run.py
+++ run.py
@@ -74,15 +74,26 @@ def calculate_surplus_data(sales_row):
 def get_last_5_entries_sales():
 
     sales = SHEET.worksheet("sales")
-    # column = sales.col_values(3)
-    # print(column)
-
-
     columns = []
     for ind in range(1,7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
-    pprint(columns)
+    return columns
+
+def calculate_stock_data(data):
+    """
+    calculate the average stock for each item type,adding 10%
+    """
+    print("calculating stock data...\n")
+    new_stock_data=[]
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average*1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
 
 def main():
     data = get_sales_data()
@@ -90,8 +101,9 @@ def main():
     update_worksheet(data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("Welcome to love sandwiches")
-# main()
-
-get_last_5_entries_sales()
+main()
