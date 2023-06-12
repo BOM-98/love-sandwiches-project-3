f9c032cca0c84a850fda62204fb5a5a17018c19d
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2023-06-12 13:30:03 +0000

    retrieve stock surplus data

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint 
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -14,7 +15,6 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 
 def get_sales_data():
-
     """
     Get sales figures input from the user.
     """
@@ -24,13 +24,14 @@ def get_sales_data():
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
-        
+
         sales_data = data_str.split(",")
         if validate_data(sales_data):
             print("Data is valid")
             break
-        
+
     return sales_data
+
 
 def validate_data(values):
     """
@@ -48,6 +49,7 @@ def validate_data(values):
 
     return True
 
+
 def update_sales_worksheet(data):
     """
     update sales worksheet 
@@ -57,7 +59,18 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updata successfully.\n")
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(data)
-print(data) 
+
+def calculate_surplus_data(sales_row):
+    print("Calculating surplus data...")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+def main():
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(data)
+    calculate_surplus_data(sales_data)
+
+print("Welcome to love sandwiches")
+main()
