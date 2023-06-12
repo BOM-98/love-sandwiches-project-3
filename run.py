eb021399c72bf2d564c78345a7c2df3a1b33b3b2
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2023-06-12 13:14:30 +0000

    update sales worksheet

--- run.py
+++ run.py
@@ -18,16 +18,19 @@ def get_sales_data():
     """
     Get sales figures input from the user.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
-    
-    sales_data = data_str.split(",")
-    print(sales_data)
-    validate_data(sales_data)
-
+        data_str = input("Enter your data here: ")
+        
+        sales_data = data_str.split(",")
+        if validate_data(sales_data):
+            print("Data is valid")
+            break
+        
+    return sales_data
 
 def validate_data(values):
     """
@@ -41,6 +44,20 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
 
-get_sales_data()
- 
+    return True
+
+def update_sales_worksheet(data):
+    """
+    update sales worksheet 
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updata successfully.\n")
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(data)
+print(data) 
